feca69cfad42e2bcfe95b7c465f4ba159defce48
Vinay Chandran <vinaychandranvs@gmail.com>
2017-05-23 20:42:58 +0530

    Disabled WebView Caches.

--- app/src/main/java/com/intellidesigns/ktyou/MainActivity.java
+++ app/src/main/java/com/intellidesigns/ktyou/MainActivity.java
@@ -612,7 +612,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mWebviewPop.setWebViewClient(new UriWebViewClient());
             mWebviewPop.getSettings().setJavaScriptEnabled(true);
             mWebviewPop.getSettings().setSavePassword(false);
-            mWebviewPop.getSettings().setAppCacheEnabled(true);
+            mWebviewPop.getSettings().setAppCacheEnabled(false);
+            mWebViewPop.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
             mWebviewPop.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
             mWebviewPop.getSettings().setSupportMultipleWindows(true);
             mWebviewPop.getSettings().setGeolocationEnabled(true);
